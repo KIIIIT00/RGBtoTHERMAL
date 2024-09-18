b57fa8cdabaeffa430b3560eea5a3a0ebb6063d4
kawa <kawahara.aislab@gmail.com>
2024-09-18 20:52:26 +0900

    CalibrtaionbeforeAfter

--- src/Others/cap.py
+++ src/Others/cap.py
@@ -2,41 +2,46 @@ import cv2
 import os
 
 # ビデオキャプチャオブジェクトを作成する（デフォルトのカメラを使用）
-#cap = cv2.VideoCapture(1)
-cam = cv2.VideoCapture(0)
+thermal = cv2.VideoCapture(0)
+rgb = cv2.VideoCapture(1)
 
-imgpath = './Calibration/'
+imgpath = './results/Calibration/beforeAfter/'
 framecount = 1
 #if not cap.isOpened():
 #    print("カメラを開くことができませんでした")
 #    exit()
+rgb.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+rgb.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+thermal.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+thermal.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 while True:
     # カメラからフレームを読み込む
-    #ret, frame = cap.read()
-    ret1, Frame = cam.read()
+    _, rgb_frame = rgb.read()
+    _, thermal_frame =thermal.read()
 
     #if not ret:
     #    print("フレームを取得できませんでした")
     #    break
 
     # フレームをウィンドウに表示する
-   # cv2.imshow('Webcam', frame)
-    cv2.imshow('Webcam2', Frame)
+    cv2.imshow('Webcam', rgb_frame)
+    cv2.imshow('Webcam2', thermal_frame)
+    
     # キャプチャ
     if cv2.waitKey(1) == ord('c'):
         print('capture')
-        rgb_filename = 'chessboard_check_'f'{framecount}''.png'
+        rgb_filename = 'after_rgb_'f'{framecount}''.png'
         print(rgb_filename)
-        thermal_filename = 'cold_thermal_'f'{framecount}''.png'
-        #cv2.imwrite(os.path.join(imgpath, rgb_filename), frame)
-        cv2.imwrite(os.path.join(imgpath, rgb_filename), Frame)
+        thermal_filename = 'after_thermal_'f'{framecount}''.png'
+        cv2.imwrite(os.path.join(imgpath, rgb_filename), rgb_frame)
+        cv2.imwrite(os.path.join(imgpath, thermal_filename), thermal_frame)
         framecount += 1
     # 'q'キーが押されたらループを終了する
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # リソースを解放する
-#cap.release()
-cam.release()
+rgb.release()
+thermal.release()
 cv2.destroyAllWindows()
